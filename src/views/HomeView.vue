9e50ba5fc45c4a33559945f7cdc5010f440745cd
Sascha Trifunovic <sascha.trifunovic@gmail.com>
2024-08-11 17:08:09 +0200

    Enable teams

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,15 @@ import { useKoreroStore } from '@/stores/korero'
 import BaseCard from '@/components/BaseCard.vue'
 import HeadingOne from '@/components/HeadingOne.vue'
 import { useRouter } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { ref, type Ref } from 'vue'
 
 const router = useRouter()
 
 const koreroStore = useKoreroStore()
-if (!koreroStore.config) {
+
+const { config, channels, orgs, user } = storeToRefs(koreroStore)
+if (!config.value) {
   // TODO:
   // * get all teams (or just org?)
   // * let user choose a team/org to build
@@ -18,19 +22,79 @@ if (!koreroStore.config) {
   // 6. If no teams with channels are found, show choose team to create channels view.
 }
 
-function chooseOrg() {
-  koreroStore.setOrg(koreroStore.user.id)
+function chooseTeam(teamId: string) {
+  koreroStore.setTeam(teamId)
+}
+
+const showSwitchTeam: Ref<boolean> = ref(false)
+function label(teamId: string) {
+  if (config.value?.currentTeam == teamId) {
+    return 'current'
+  }
+  if (config.value?.teams.includes(teamId)) {
+    return 'active'
+  }
+  return 'unused'
 }
 </script>
 
 <template>
   <main>
-    <div v-if="!koreroStore.config" class="text-center">
-      <HeadingOne class="text-center pb-6">Get started</HeadingOne>
-      <button @click="chooseOrg" class="btn btn-accent">Choose organization</button>
+    <div v-if="!config">
+      <HeadingOne class="pb-6">Get started</HeadingOne>
+
+      <p class="font-bold">{{ user.name }}</p>
+      <ul>
+        <li>
+          Just me
+          <button @click="chooseTeam(user.id)" class="btn btn-accent">Choose team</button>
+        </li>
+      </ul>
+      <div v-for="org in orgs">
+        <p class="font-bold">{{ org.name }}</p>
+        <ul>
+          <li v-for="team in org.teams">
+            {{ team.name }}
+            <button @click="chooseTeam(team.id)" class="btn btn-accent">Choose team</button>
+          </li>
+        </ul>
+      </div>
     </div>
 
     <div v-else>
+      <button @click="showSwitchTeam = true" class="btn btn-accent">Switch team</button>
+      <div v-if="showSwitchTeam">
+        <p class="font-bold">{{ user.name }}</p>
+        <ul>
+          <li>
+            Just me {{ label(user.id) }}
+            <button
+              v-if="config.currentTeam !== user.id"
+              @click="chooseTeam(user.id)"
+              class="btn btn-accent"
+            >
+              Choose team
+            </button>
+          </li>
+        </ul>
+        <div v-for="org in orgs">
+          <p class="font-bold">{{ org.name }}</p>
+          <ul>
+            <li v-for="team in org.teams">
+              {{ team.name }} {{ label(team.id) }}
+              <button
+                v-if="config.currentTeam !== team.id"
+                @click="chooseTeam(team.id)"
+                class="btn btn-accent"
+              >
+                Choose team
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <button @click="showSwitchTeam = false" class="btn btn-accent">Cancel</button>
+      </div>
       <div class="flex gap-4 justify-between items-center">
         <HeadingOne>Channels</HeadingOne>
         <div>
@@ -40,12 +104,10 @@ function chooseOrg() {
         </div>
       </div>
 
-      <p v-if="koreroStore.channels.length === 0" class="text-center py-12">
-        Create a channel to get started.
-      </p>
+      <p v-if="channels.length === 0" class="text-center py-12">Create a channel to get started.</p>
 
       <div class="grid grid-cols-3 gap-4 py-4">
-        <BaseCard v-for="channel in koreroStore.channels" :key="channel.id" class="mb-4">
+        <BaseCard v-for="channel in channels" :key="channel.id" class="mb-4">
           <h2 class="card-title">
             <RouterLink :to="{ name: 'channel', params: { channelId: channel.id } }">{{
               channel.name
