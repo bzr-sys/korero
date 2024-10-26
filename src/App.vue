d3e28777b37b159b4b8b7d4ca1e32a921f87128c
Paddy <ohanlon.paddy@gmail.com>
2024-10-26 11:33:54 +0200

    Show loading icon when loading

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar.vue'
 import BaseLogin from './components/BaseLogin.vue'
 import SideBar from './components/SideBar.vue'
 import BaseOnboarding from './components/BaseOnboarding.vue'
+import LoaderSVG from './components/LoaderSVG.vue'
 
 const koreroStore = useKoreroStore()
 koreroStore.autoSignIn()
@@ -17,28 +18,33 @@ bzr.onLogin(async () => {
 </script>
 
 <template>
-  <div
-    v-if="koreroStore.authenticated"
-    class="flex flex-col overflow-hidden fixed top-0 right-0 bottom-0 left-0 min-h-[600px]"
-  >
-    <NavBar />
-    <template v-if="!koreroStore.hasCompletedOnboarding">
-      <BaseOnboarding />
-    </template>
-    <template v-if="koreroStore.state">
-      <div class="flex flex-1">
-        <SideBar />
-        <!-- height = 100vh - navbar header height -->
-        <main class="flex-grow h-[calc(100vh-12*0.25rem)] overflow-hidden">
-          <div class="px-6 pt-4 pb-16 overflow-y-auto h-full">
-            <RouterView />
-          </div>
-        </main>
-      </div>
-    </template>
+  <div v-if="!koreroStore.loaded" class="grid w-full justify-center items-center h-full">
+    <LoaderSVG width="40px" />
   </div>
   <template v-else>
-    <BaseLogin />
+    <div
+      v-if="koreroStore.authenticated"
+      class="flex flex-col overflow-hidden fixed top-0 right-0 bottom-0 left-0 min-h-[600px]"
+    >
+      <NavBar />
+      <template v-if="!koreroStore.hasCompletedOnboarding">
+        <BaseOnboarding />
+      </template>
+      <template v-if="koreroStore.state">
+        <div class="flex flex-1">
+          <SideBar />
+          <!-- height = 100vh - navbar header height -->
+          <main class="flex-grow h-[calc(100vh-12*0.25rem)] overflow-hidden">
+            <div class="px-6 pt-4 pb-16 overflow-y-auto h-full">
+              <RouterView />
+            </div>
+          </main>
+        </div>
+      </template>
+    </div>
+    <template v-else>
+      <BaseLogin />
+    </template>
   </template>
 </template>
 
